0a4903788d5fbbffe433d159d614b48959aa44f4
Anonyme <Anonyme>
2026-04-17 23:00:04 +0200

    4 sur 5 taches

--- db_init.js
+++ db_init.js
@@ -2,8 +2,9 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false } 
-  }
+    ssl: { rejectUnauthorized: false }, 
+  },
+  pool: { min: 2, max: 10 }
 });
 
 async function initDB() {
--- serveur.js
+++ serveur.js
@@ -14,8 +14,9 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false } 
-  }
+    ssl: { rejectUnauthorized: false }, 
+  },
+  pool: { min: 2, max: 10 }
 });
 
 
